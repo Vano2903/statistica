9341acdc638bef0767c1456996bb4ca5e5c59f96
davide vanoncini <davidevanoncini2003@gmail.com>
2021-10-11 22:29:15 +0200

    working on getting all the students, and accessing the file in a direct access

--- fileHandler.go
+++ fileHandler.go
@@ -1,6 +1,11 @@
 package main
 
-import "os"
+import (
+	"bytes"
+	"encoding/binary"
+	"os"
+	"unsafe"
+)
 
 type FileHandler struct {
 	Path string
@@ -24,6 +29,35 @@ func (f FileHandler) Append(b []byte) error {
 	return nil
 }
 
-func (f FileHandler) GetAllStudents() ([]Student, error) {
-	return nil, nil
+func readNextBytes(file *os.File, recordSize, startFrom int) ([]byte, error) {
+	bytes := make([]byte, recordSize)
+
+	_, err := file.ReadAt(bytes, int64(startFrom))
+	if err != nil {
+		return nil, err
+	}
+
+	return bytes, nil
+}
+
+func (f FileHandler) GetAllStudents() (Student, error) {
+	var s Student
+	size := int(unsafe.Sizeof(s))
+	file, err := os.Open(f.Path)
+	if err != nil {
+		return Student{}, err
+	}
+	defer file.Close()
+
+	data, err := readNextBytes(file, size, size*1)
+	if err != nil {
+		return Student{}, err
+	}
+	buffer := bytes.NewBuffer(data)
+	err = binary.Read(buffer, binary.BigEndian, &s)
+	if err != nil {
+		return Student{}, err
+	}
+
+	return s, nil
 }
--- main.go
+++ main.go
@@ -4,29 +4,41 @@ import (
 	"bytes"
 	"encoding/gob"
 	"fmt"
+	"log"
 )
 
 const filePath string = "statistica.bin"
 
 type Student struct {
-	LastName    [20]rune
-	Name        [20]rune
-	Phone       [13]rune
-	Email       [25]rune
-	HasLaptop   bool
-	SummerStage bool
+	LastName    [20]byte
+	Name        [20]byte
+	Phone       [13]byte
+	Email       [25]byte
+	HasLaptop   byte
+	SummerStage byte
 }
 
-func (s Student) Encode() ([]byte, error) {
-	var buff bytes.Buffer
-	enc := gob.NewEncoder(&buff)
-	err := enc.Encode(s)
-	if err != nil {
-		return nil, err
+//convert the student struct to slice of byte
+func (s Student) Encode() []byte {
+	var complete []byte
+	for _, b := range s.LastName {
+		complete = append(complete, b)
 	}
-	return buff.Bytes(), nil
+	for _, b := range s.Name {
+		complete = append(complete, b)
+	}
+	for _, b := range s.Phone {
+		complete = append(complete, b)
+	}
+	for _, b := range s.Email {
+		complete = append(complete, b)
+	}
+	complete = append(complete, s.HasLaptop)
+	complete = append(complete, s.SummerStage)
+	return complete
 }
 
+//given a slice of byte it will try to convert it to the student struct
 func (s *Student) Decode(b []byte) error {
 	var buff bytes.Buffer
 	dec := gob.NewDecoder(&buff)
@@ -37,21 +49,79 @@ func (s *Student) Decode(b []byte) error {
 	return nil
 }
 
+//print a student in a formatted way
+func (s Student) Print() {
+	fmt.Print("nome: ")
+	for _, a := range s.Name {
+		fmt.Print(string(a))
+	}
+	fmt.Println("")
+
+	fmt.Print("cognome: ")
+	for _, a := range s.LastName {
+		fmt.Print(string(a))
+	}
+	fmt.Println("")
+
+	fmt.Print("telefono: ")
+	for _, a := range s.Phone {
+		fmt.Print(string(a))
+	}
+	fmt.Println("")
+
+	fmt.Print("email: ")
+	for _, a := range s.Email {
+		fmt.Print(string(a))
+	}
+	fmt.Println("")
+
+	fmt.Print("ha un laptop: ")
+	if string(s.HasLaptop) == "t" {
+		fmt.Print("si")
+	} else {
+		fmt.Print("no")
+	}
+	fmt.Println("")
+
+	fmt.Print("ha fatto lo stage: ")
+	if string(s.SummerStage) == "t" {
+		fmt.Print("si")
+	} else {
+		fmt.Print("no")
+	}
+	fmt.Println("")
+}
+
 func main() {
 	var s Student
-	s.Name = [20]rune{'a', 'a', 'a', 'a', 'a'}
-	s.LastName = [20]rune{'b', 'b', 'b', 'b', 'b'}
-	s.Phone = [13]rune{'c', 'c', 'c', 'c', 'c'}
-	s.Email = [25]rune{'d', 'd', 'd', 'd', 'd'}
-	s.HasLaptop = true
-	s.SummerStage = true
+	s.Name = [20]byte{'a', 'a', 'a', 'a', 'a'}
+	s.LastName = [20]byte{'b', 'b', 'b', 'b', 'b'}
+	s.Phone = [13]byte{'c', 'c', 'c', 'c', 'c'}
+	s.Email = [25]byte{'d', 'd', 'd', 'd', 'd'}
+	s.HasLaptop = byte('t')
+	s.SummerStage = byte('f')
 
-	a, _ := s.Encode()
-	fmt.Println(a)
+	a := s.Encode()
+
+	s.Name = [20]byte{'A', 'A', 'A', 'A', 'A'}
+	s.LastName = [20]byte{'B', 'B', 'B', 'B', 'B'}
+	s.Phone = [13]byte{'C', 'C', 'C', 'C', 'C'}
+	s.Email = [25]byte{'D', 'D', 'D', 'D', 'D'}
+	s.HasLaptop = byte('f')
+	s.SummerStage = byte('t')
 
 	f := NewFileHandler()
 	f.Path = filePath
 
 	f.Append(a)
 
+	b := s.Encode()
+	f.Append(b)
+
+	s, err := f.GetAllStudents()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s.Print()
 }
