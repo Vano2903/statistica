27d3ea144e7c70dad145e3fc46bd74df25a8e16d
Vano2903 <davidevanoncini2003@gmail.com>
2021-10-14 12:28:02 +0200

    removed manu func

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func init() {
 	}
 }
 
-func menu() {
+func main() {
 	for {
 		clear.Clear()
 		fmt.Println("STATISTICA - VANONCINI && MORANDI")
@@ -59,36 +59,4 @@ func menu() {
 			return
 		}
 	}
-}
-
-func main() {
-	menu()
-	// var s Student
-
-	// s.Name = [20]byte{'A', 'A', 'A', 'A', 'A'}
-	// s.LastName = [20]byte{'B', 'B', 'B', 'B', 'B'}
-	// s.Phone = [13]byte{'0', '3', '5', '2', '3', '8', '2', '3', '4'}
-	// s.Email = [25]byte{'n', 'n', 'n', 'n', 'n'}
-	// s.HasLaptop = byte('f')
-	// s.SummerStage = byte('t')
-
-	// a := s.Encode()
-
-	// f.Append(a)
-
-	// s.Name = [20]byte{'a', 'a', 'a', 'a', 'a'}
-	// s.LastName = [20]byte{'b', 'b', 'b', 'b', 'b'}
-	// s.Phone = [13]byte{'3', '4', '7', '8', '2', '5', '7', '0', '7', '6'}
-	// s.Email = [25]byte{'d', 'd', 'd', 'd', 'd'}
-	// s.HasLaptop = byte('t')
-	// s.SummerStage = byte('f')
-
-	// b := s.Encode()
-	// f.Append(b)
-
-	// s, err := f.SearchByPhone("3478257076")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// s.Print()
 }
